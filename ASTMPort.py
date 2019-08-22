2321475d36f37ff3193163784fcd8ecf38359858
FeedFish34 <1073452867@qq.com>
2019-08-22 19:00:28 +0800

    ASTM通讯

--- ASTMPort.py
+++ ASTMPort.py
@@ -9,6 +9,7 @@ DATE_FORMAT = "%m/%d/%Y %H:%M:%S %p"                        # 日期格式
 fp = logging.FileHandler('Debug.txt', encoding='utf-8')
 fs = logging.StreamHandler()
 logging.basicConfig(level=logging.DEBUG, format=LOG_FORMAT, datefmt=DATE_FORMAT, handlers=[fp, fs])
+List = []
 
 def recv(serial):
     while True:
@@ -31,30 +32,30 @@ if __name__ == '__main__':
     while True:
         data =recv(serial)
         if(data != b''):
-            print(str(data))
+            print(str(resMsg))
             result = 'x04' in str(data)
             print(result)
             if(result):
                 print("receive : ",data)
                 logging.debug("接收：" + data.decode('utf-8'))
-                for s in data:
-                    if(s == 5 or s == 2 or s == 3):
-                        logging.debug("发送：" + chr(int("06")))
-                        serial.write(bytearray(chr(int("06")), encoding="utf-8"))  # 数据写回
                 EnqString = chr(int("05"))
                 StxString = chr(int("02"))
                 EtxString = chr(int("03"))
                 EotString = chr(int("04"))
                 print(data.decode('utf-8'))
                 data = data.decode('utf-8')
-                resMsg = re.findall(r'\b\d+\b', data)
+                resMsg = re.findall(r'\b\d+\b', resMsg)
                 resMsg = str(resMsg).replace("['","").rstrip("']")
                 resResult = str(random.randint(0,999));
                 print(resMsg)
                 sampId = str(resMsg)
-                resMsg = EnqString + StxString + str(resMsg) + "-" + resResult + EtxString + EotString
-                serial.write(bytearray(resMsg, encoding="utf-8"))  # 数据写回
-                logging.debug("发送：" + resMsg)
+                List.append(EnqString)
+                List.append(StxString + str(resMsg) + "-" + resResult + EtxString)
+                List.append(EotString)
+                #resMsg = EnqString + StxString + str(resMsg) + "-" + resResult + EtxString + EotString
+                logging.debug("发送1：" + List[0])
+                serial.write(bytes(List[0].encode('utf-8'))) # 数据写回
+                List.pop(0)
 
                 cx = sqlite3.connect("C:/Users/Administrator/PycharmProjects/ASTMSerial/LocalData.db")
                 cu = cx.cursor()
@@ -64,4 +65,16 @@ if __name__ == '__main__':
                 resMsg = ""
                 resResult = ""
             else:
-                resMsg = data.decode('utf-8') + resMsg
+                for s in data:
+                    if(s == 6 and len(List) > 0):
+                        logging.debug("发送2：" + List[0])
+                        serial.write(bytes(List[0].encode('utf-8')))  # 数据写回
+                        List.pop(0)
+                    elif(s == 5 or s == 2 or s == 3):
+                        logging.debug("发送3：" + chr(int("06")))
+                        serial.write(bytearray(chr(int("06")), encoding="utf-8"))  # 数据写回
+                if(data[0] == 6):
+                     print("只接收到ACK")
+                     logging.debug("接收：" + data.decode('utf-8'))
+                else:
+                    resMsg = data.decode('utf-8') + resMsg
